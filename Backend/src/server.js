5c72342df4116b853aae863a32ef354115bc384c
Victor Nunez <tu_correo@gmail.com>
2026-03-21 17:35:27 -0300

    feat: integrar rutas de ordenes, configurar Pug como motor de vistas y habilitar method-override para formularios

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,15 +2,27 @@ import express from 'express';
 import mongoose from 'mongoose';
 import productRoutes from './routes/productsRoutes.js';
 import adminRoutes from './routes/adminroutes.js';
-import { PORT } from './config.js';
+import { PORT, DB_URI } from './config.js';
 import { createSession } from './middleware/session.js';
+import orderRoutes from './routes/ordersRoutes.js';
+import methodoverride from 'method-override';
 
 const app = express();
-const DB_URI ='mongodb://localhost:27017/web_repository';
+
+// Le decimos a express en donde estan las vistas(Carpeta)
+app.set('views', './views');
+
+// Le decimos que el motor de renderizado es Pug
+app.set('view engine', 'pug');
+
 
 // middleware que traduce al servidor los archivos JSON
 app.use(express.json());
+// Sirve para que el servidor entienda los datos que vengan de un formulario
 app.use(express.urlencoded({ extended: true }));
+// Permite que el metodo "POST" de un formulario se convierta en PUT o DELETE usando "_method"
+app.use(methodoverride('_method'));
+
 
 // Usamos sessiones para guardar la sesion del usuario.
 app.use(createSession)
@@ -18,6 +30,7 @@ app.use(createSession)
 // Asignamos las rutas del servidor.
 app.use('/api/products', productRoutes);
 app.use('/auth', adminRoutes);
+app.use('/api/orders', orderRoutes);
 
 
 // Conectamos nuestro servidor a la base de datos
