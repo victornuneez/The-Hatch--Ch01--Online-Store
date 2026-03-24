5273c215c726449aecb78aeff020815d2b3d2710
Victor Nunez <tu_correo@gmail.com>
2026-03-24 16:34:51 -0300

    feat: agregar ruta patch para cambiar el estado del producto

--- Backend/src/routes/productsRoutes.js
+++ Backend/src/routes/productsRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createProduct, getProducts, getProductById, updateProductById, deleteProductById } from '../controller/productsController.js';
+import { createProduct, getProducts, getProductById, updateProductById, updateStatusProduct, deleteProductById } from '../controller/productsController.js';
 import { sessionVerify } from '../middleware/authSession.js';
 import { verifyRole } from '../middleware/verifyRole.js';
 
@@ -9,6 +9,7 @@ router.post('/', sessionVerify, verifyRole, createProduct);
 router.get('/', sessionVerify, verifyRole, getProducts);
 router.get('/edit/:id', sessionVerify, verifyRole, getProductById);
 router.put('/:id', sessionVerify, verifyRole, updateProductById);
+router.patch('/:id/status', sessionVerify, verifyRole, updateStatusProduct);
 router.delete('/:id', sessionVerify, verifyRole, deleteProductById);
 
 export default router;
