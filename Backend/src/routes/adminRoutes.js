84a4195a5c49e81ccf0825e0e58465b717d49ea3
Victor Nunez <tu_correo@gmail.com>
2026-03-26 23:53:58 -0300

    refactor: cambiar metodo de logout de GET a POST por seguridad

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -7,6 +7,6 @@ const router = Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/login', viewLogin);
-router.get('/logout', logout); 
+router.post('/logout', logout); 
 
 export default router; 
